986074acf7a85b0c03f26a05a3731294a6ff07ed
David Houston <davidaehouston@gmail.com>
2025-08-14 19:06:40 +0100

    Adding Menu & Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
-import klr_logo from '../assets/klr_logo.png'
+import klr_logo from '../assets/klr_logo.png';
+import Menu from './Menu';
 
 const Header = () => {
     return (
-        <div className="header flex items-center gap-7">
-            <img src={klr_logo} className="logo scale-250" alt="KLR Logo" />
+        <div className="header flex items-center m-5 gap-4">
+            <Menu />
+            <img src={klr_logo} className="logo h-20" alt="KLR Logo" />
             <div className="flex flex-col">
                 <h1 className="font-serif text-3xl font-extrabold text-green-900 pt-0">Kristen Robbins</h1>
-                <h4 className="font-serif text-left text-2xl font-medium text-green-900">Psychotherapy & <br></br> Counselling</h4>
+                <h4 className="font-serif text-left text-2xl font-medium text-green-900">Psychotherapy & <br /> Counselling</h4>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
